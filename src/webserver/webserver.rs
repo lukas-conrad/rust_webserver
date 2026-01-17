fba42f7675efccb87050378c52e60decbea82715
UnleqitDEV <conradlu@online.de>
2026-01-17 23:24:41 +0100

    feat: enhance request handling in webserver with improved error handling and base64 encoding

--- src/webserver/webserver.rs
+++ src/webserver/webserver.rs
@@ -1,25 +1,30 @@
+use crate::plugin::plugin_manager::PluginError;
+use crate::plugin_old::models::{HttpHeader, HttpRequest, HttpResponse};
+use base64::prelude::BASE64_STANDARD;
+use base64::Engine;
+use bytes::Bytes;
+use futures::future::BoxFuture;
+use http_body_util::{BodyExt, Full};
+use hyper::body::Incoming;
+use hyper::service::Service;
+use hyper::{Request, Response};
 use std::convert::Infallible;
 use std::sync::Arc;
-use bytes::Bytes;
-use crate::plugin_old::models::{HttpHeader, HttpRequest, HttpResponse};
-use futures::future::BoxFuture;
-use http_body_util::Full;
-use hyper::body::Incoming;
-use hyper::{Request, Response};
-use hyper::service::Service;
 use tokio::sync::Mutex;
 
-pub type CallbackFn =
-    Box<dyn Fn(HttpRequest) -> BoxFuture<'static, HttpResponse> + Send + Sync + Unpin>;
+pub type CallbackFn = Box<
+    dyn Fn(HttpRequest) -> BoxFuture<'static, Result<HttpResponse, PluginError>>
+        + Send
+        + Sync
+        + Unpin,
+>;
 
 pub trait Webserver {
-
     fn set_listener(listener: CallbackFn);
-
 }
 
-pub struct DefaultWebserver{
-    listener: Arc<Mutex<Option<CallbackFn>>>
+pub struct DefaultWebserver {
+    listener: Arc<Mutex<Option<CallbackFn>>>,
 }
 
 impl Service<Request<Incoming>> for DefaultWebserver {
@@ -50,7 +55,21 @@ impl Service<Request<Incoming>> for DefaultWebserver {
                         value: value.to_str().unwrap_or("").to_string(),
                     })
                     .collect();
+                let (_, body) = req.into_parts();
+                let body_bytes = body.collect().await.unwrap_or_default().to_bytes();
 
+                let request = HttpRequest {
+                    request_method: method,
+                    path,
+                    host,
+                    headers,
+                    body: BASE64_STANDARD.encode(body_bytes),
+                };
+
+                let response_result = listener(request).await;
+                if let Ok(response) = response_result {
+
+                }
             }
             todo!();
         })

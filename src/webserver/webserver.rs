ac5ca34cf29cc490589d7533555280f6bdc40798
UnleqitDEV <conradlu@online.de>
2026-01-18 19:38:16 +0100

    feat: implement asynchronous server start and connection handling in Http1Server

--- src/webserver/webserver.rs
+++ src/webserver/webserver.rs
@@ -1,5 +1,6 @@
 use crate::plugin::plugin_manager::PluginError;
 use crate::plugin_old::models::{HttpHeader, HttpRequest, HttpResponse};
+use crate::webserver_old::WebServerService;
 use base64::engine::general_purpose;
 use base64::prelude::BASE64_STANDARD;
 use base64::Engine;
@@ -7,12 +8,16 @@ use bytes::Bytes;
 use futures::future::BoxFuture;
 use http_body_util::{BodyExt, Full};
 use hyper::body::Incoming;
+use hyper::server::conn::http1;
 use hyper::service::Service;
 use hyper::{Request, Response, StatusCode};
-use log::error;
+use hyper_util::rt::TokioIo;
+use log::{error, info};
 use std::convert::Infallible;
+use std::net::SocketAddr;
 use std::sync::Arc;
 use strum::Display;
+use tokio::net::TcpListener;
 use tokio::sync::Mutex;
 
 #[derive(Display, Debug)]
@@ -36,6 +41,27 @@ pub struct Http1Server {
 }
 
 impl Http1Server {
+    async fn start(addr: SocketAddr) -> Result<Arc<Self>, std::io::Error> {
+        let server = Arc::new(Self {
+            listener: Arc::new(Mutex::new(None)),
+        });
+
+        let listener = TcpListener::bind(addr).await?;
+
+        let (stream, _) = listener.accept().await?;
+        let io = TokioIo::new(stream);
+
+        let service = server.clone();
+
+        tokio::task::spawn(async move {
+            if let Err(err) = http1::Builder::new().serve_connection(io, service).await {
+                error!("Connection error: {:?}", err);
+            }
+        });
+
+        Ok(server)
+    }
+
     async fn build_http_request(req: Request<Incoming>) -> HttpRequest {
         let method = req.method().as_str().to_string();
         let path = req.uri().path().to_string();
@@ -71,7 +97,7 @@ impl Http1Server {
 
 impl Webserver for Http1Server {
     fn set_listener(&self, listener: CallbackFn) {
-        let web_listener= self.listener.clone();
+        let web_listener = self.listener.clone();
         tokio::spawn(async move {
             let _ = web_listener.lock().await.insert(listener);
         });
@@ -156,5 +182,70 @@ impl Service<Request<Incoming>> for Http1Server {
                 }
             }
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::plugin_old::models::{HttpRequest, HttpResponse};
+    use crate::webserver::webserver::{Http1Server, Webserver};
+    use futures::FutureExt;
+    use std::net::SocketAddr;
+    use std::sync::Arc;
+    use bytes::Bytes;
+    use hyper::Request;
+    use tokio::sync::{oneshot, Mutex};
+
+    #[tokio::test]
+    async fn test_webserver() {
+        let server: Arc<dyn Webserver> = Http1Server::start(SocketAddr::from(([0, 0, 0, 0], 60724)))
+            .await
+            .unwrap();
+
+        let (sender, receiver) = oneshot::channel::<HttpRequest>();
+        let sender = Arc::new(Mutex::new(Some(sender)));
+        server.set_listener(Box::new(move |request| {
+            let sender = sender.clone();
+            async move {
+                sender.lock().await.take().unwrap().send(request).unwrap();
+                Ok(HttpResponse {
+                    headers: vec![],
+                    status_code: 200,
+                    body: "".to_string(),
+                })
+            }
+            .boxed()
+        }));
+
+
+        // TCP-Verbindung zum Server aufbauen
+        let stream = tokio::net::TcpStream::connect("127.0.0.1:8080")
+            .await
+            .unwrap();
+        let io = hyper_util::rt::TokioIo::new(stream);
+
+        // HTTP-Client-Verbindung erstellen
+        let (mut sender, conn) = hyper::client::conn::http1::handshake(io).await.unwrap();
+
+        // Connection in eigenem Task laufen lassen
+        tokio::task::spawn(async move {
+            if let Err(err) = conn.await {
+                eprintln!("Connection failed: {:?}", err);
+            }
+        });
+
+        // Request senden
+        let req = Request::builder()
+            .uri("/test")
+            .method("GET")
+            .body(http_body_util::Empty::<Bytes>::new())
+            .unwrap();
+
+        let _res = sender.send_request(req).await.unwrap();
+
+        let request = receiver.await.unwrap();
+
+        assert_eq!(request.body, "");
+
     }
 }
